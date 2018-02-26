c1ba2cc85341d5f3190053970e38117290fd4310
YanC Lin <yonalin1@gmail.com>
2018-02-26 19:51:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,8 @@ new_student # return the newly created instance
        FROM students
        WHERE grade = 9
      SQL
-
+     DB[:conn].execute(sql, name).map do |row|
+       self.new_from_db(row)
   end
 
   def save
